f96a039fbedcd4da8d2827d89e223ef50e43f150
Domingos Manuel Ifafe <ifafe@Domingoss-MacBook-Air.local>
2025-04-13 03:17:51 +0100

    Initial commit

---
+++ src/main/java/com/mwana/MwanaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mwana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwanaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwanaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwana/MwanaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwanaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
